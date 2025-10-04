92a40423b2d25574693d40d54a462facea6896c0
reda201412 <redabstam@gmail.com>
2025-10-04 19:29:58 +0200

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import { LaunchOfferBar } from "@/components/LaunchOfferBar";
 import ScrollToTop from "@/components/ScrollToTop";
 import Home from "./pages/Home";
@@ -28,6 +29,7 @@ const App = () => (
       <LaunchOfferBar />
       <Toaster />
       <Sonner />
+      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/produit/:id" element={<Product />} />
